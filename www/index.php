e11898f513dd5a994b09029025aee61c6018f725
qiujunli <qiujunli@xiaomi.com>
2015-11-06 20:38:12 +0800

    优化代码

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@
  * 项目入口
  * 定义项目名称，项目名称与Core在同一目录下
  */
-define("CORE_PATH",  realpath(dirname(__FILE__) . '/../'));
+define("ROOT_PATH",  realpath(dirname(__FILE__) . '/../'));
 define('APP_NAME', 'APP');
-require(CORE_PATH."/Core/index.php");
+require(ROOT_PATH."/Core/index.php");
 ?>
